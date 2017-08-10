ca10eb1d2089586e05862cb754f23e7bec5316d4
Hong <jimmy600kimo@hotmail.com>
2017-08-10 18:20:06 +0800

    refactor(addressAutoComplete): merge into InputAddresForm

--- src/components/UbikeDirection/InputAddressForm.jsx
+++ src/components/UbikeDirection/InputAddressForm.jsx
@@ -1,12 +1,88 @@
 import React, {Component} from 'react';
-import {initialAutocomplete} from '../../../addressAutoComplete';
 import {initialStartDirect} from "../../../direction";
-
+import location from '../../../locationData';
 
 class InputAddressForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.autocomplete = {};
+        this.autocompleteDestination = {};
+    }
+
+    initAutocomplete() {
+        // Create the autocomplete object, restricting the search to geographical
+        // location types.
+        this.autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+            {types: ['geocode']});
+
+        this.autocompleteDestination = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById('autocomplete-destination')),
+            {types: ['geocode']});
+
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+        this.autocomplete.addListener('place_changed', this.getOriginLocation.bind(this));
+        this.autocompleteDestination.addListener('place_changed', this.getDestinationLocation.bind(this));
+
+    }
+
+    getOriginLocation() {
+        // Get the place details from the autocomplete object.
+        let place = this.autocomplete.getPlace();
+
+
+        if (place) {
+            location.origin.lat = place.geometry.location.lat();
+            location.origin.lng = place.geometry.location.lng();
+            console.log('lat ' + location.origin.lat);
+            console.log('lng ' + location.origin.lng);
+        }
+    }
+
+    getDestinationLocation() {
+        // Get the place details from the autocomplete object.
+        let place = this.autocompleteDestination.getPlace();
+
+        if (place) {
+            location.destination.lat = place.geometry.location.lat();
+            location.destination.lng = place.geometry.location.lng();
+            console.log('lat ' + location.destination.lat);
+            console.log('lng ' + location.destination.lng);
+        }
+    }
+
+
+
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+    geolocate() {
+        let that = this;
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                let geolocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                let circle = new google.maps.Circle({
+                    center: geolocation,
+                    radius: position.coords.accuracy
+                });
+                that.autocomplete.setBounds(circle.getBounds());
+            });
+        }
+    }
+
+    initialAutocomplete() {
+        this.initAutocomplete();
+        $("#autocomplete").focus(this.geolocate.bind(this));
+    }
+
+
 
     componentDidMount() {
-       initialAutocomplete();
+       this.initialAutocomplete();
        initialStartDirect();
     }
 
